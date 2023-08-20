a421be6855ffb4fbe6ffe757c72862fa6ddda0fe
adrian-siv <adrian.ssiv@gmail.com>
2023-08-20 17:14:34 -0300

    feat: finish ex

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -3,6 +3,7 @@ package com.example;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class PrimaryController {
@@ -11,11 +12,28 @@ public class PrimaryController {
     TextField txtProduto;
     @FXML
     TextField txtSetor;
+    @FXML
+    ListView<Produto> listProdutos;
     
     private ArrayList<Produto> produtos = new ArrayList<>();
     
-    public void adicionar() {
+    public void adicionarProduto() {
         var produto = new Produto(txtProduto.getText(), txtSetor.getText());
+        produtos.add(produto);
+        txtProduto.clear();
+        txtSetor.clear();
+        
+        mostrarProduto();
+    }
+    
+    public void mostrarProduto() {
+        listProdutos.getItems().clear();
+        
+        produtos.sort((p1,p2) -> p1.setor().compareToIgnoreCase(p2.setor()));
+
+        for (var produto : produtos) {
+            listProdutos.getItems().add(produto);
+        }
 
     }
 }
